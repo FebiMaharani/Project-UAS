458ee78e75b3fba99eb9001b08f5ad9db631efa5
FebiMaharani <147477650+FebiMaharani@users.noreply.github.com>
2024-12-28 19:23:27 +0700

    Update DetailsUiState.kt

--- app/src/main/java/id/febimaharani/bukuku/ui/screens/detail_screen/DetailsUiState.kt
+++ app/src/main/java/id/febimaharani/bukuku/ui/screens/detail_screen/DetailsUiState.kt
@@ -2,6 +2,7 @@ package id.febimaharani.bukuku.ui.screens.detail_screen
 
 import id.febimaharani.bukuku.model.Book
 
+// menampilkan setail status ui (loading, eror, menyimpan buku)
 sealed interface DetailsUiState {
     data class Success(val bookItem: Book) : DetailsUiState
     object Error : DetailsUiState
