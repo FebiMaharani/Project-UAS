f9bca0170cb0ba6358f5df58111c763ed06fae95
FebiMaharani <147477650+FebiMaharani@users.noreply.github.com>
2024-12-28 18:14:57 +0700

    Update BukuKuRepository.kt

--- app/src/main/java/id/febimaharani/bukuku/data/BukuKuRepository.kt
+++ app/src/main/java/id/febimaharani/bukuku/data/BukuKuRepository.kt
@@ -2,12 +2,9 @@ package id.febimaharani.bukuku.data
 
 import id.febimaharani.bukuku.model.Book
 
-/**
- * Repository retrieves volume data from underlying data source.
- */
+// Repository
 interface BukuKuRepository {
-    /** Retrieves list of books from underlying data source */
-    // Notes: List<Book>? NULLABLE
+    // mengambil data buku dari url network
     suspend fun getBooks(query: String): List<Book>?
 
     suspend fun getBook(id: String): Book?
