2218e10a98e09a8e7d703cf3d11f1519d4614359
FebiMaharani <147477650+FebiMaharani@users.noreply.github.com>
2024-12-28 18:18:03 +0700

    Update DefaultBukuKuRepository.kt

--- app/src/main/java/id/febimaharani/bukuku/data/DefaultBukuKuRepository.kt
+++ app/src/main/java/id/febimaharani/bukuku/data/DefaultBukuKuRepository.kt
@@ -3,14 +3,11 @@ package id.febimaharani.bukuku.data
 import id.febimaharani.bukuku.model.Book
 import id.febimaharani.bukuku.network.BukuKuApiService
 
-/**
- * Default Implementation of repository that retrieves volumes data from underlying data source.
- */
+// implementasi repository default volume data dari sumber data (network)
 class DefaultBukuKuRepository(
     private val bukuKuApiService: BukuKuApiService
 ) : BukuKuRepository {
-    /** Retrieves list of Volumes from underlying data source */
-    // Notes: List<Book>? NULLABLE
+    // Mengambil daftar buku dari sumber data (network) 
     override suspend fun getBooks(query: String): List<Book>? {
         return try {
             val res = bukuKuApiService.getBooks(query)
