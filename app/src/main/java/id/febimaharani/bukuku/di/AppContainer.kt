1c8ef65316f556db273ca4648734d20e4bd24f9f
FebiMaharani <147477650+FebiMaharani@users.noreply.github.com>
2024-12-28 18:49:49 +0700

    Update AppContainer.kt

--- app/src/main/java/id/febimaharani/bukuku/di/AppContainer.kt
+++ app/src/main/java/id/febimaharani/bukuku/di/AppContainer.kt
@@ -3,8 +3,11 @@ package id.febimaharani.bukuku.di
 import id.febimaharani.bukuku.data.BukuKuRepository
 import id.febimaharani.bukuku.network.BukuKuApiService
 
+// TODO sebagai kontainer Dependency Injection (Di) aplikasi  
 // kontainer Dependency Injection aplikasi
 interface AppContainer {
+    // TODO menyediakan instance dari BukuKuApiService
     val bukuKuApiService: BukuKuApiService
+    // TODO menyediakan instance dari BukuKuRepository
     val bukukuRepository: BukuKuRepository
 }
